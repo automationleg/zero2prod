277812581c2cc17d3bb975cf68e6d0e75783b59a
automationleg <automationleg@gmail.com>
2022-07-19 22:42:54 +0200

    applied cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use zero2prod::run;
 use std::net::TcpListener;
-
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     run(TcpListener::bind("127.0.0.1:0")?)?.await
 }
-
