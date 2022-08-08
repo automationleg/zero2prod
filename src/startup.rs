3924df6469c8b7d24d72bc9224d6ad40a5084cba
automationleg <automationleg@gmail.com>
2022-08-08 16:15:58 +0200

    Add logging

--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,7 @@ use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
 use crate::routes::*;
 use sqlx::PgPool;
+use actix_web::middleware::Logger;
 
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || { 
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
