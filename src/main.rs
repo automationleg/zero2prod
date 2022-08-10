63fdca8435b3403ada1c7d1782d1e03090bf8f69
automationleg <automationleg@gmail.com>
2022-08-10 16:33:29 +0200

    add logs for integration tests

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
-use env_logger::Env;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use tracing_log::LogTracer;
+
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    LogTracer::init().expect("Failed to set logger");
 
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection = PgPool::connect(&configuration.database.connection_string())
